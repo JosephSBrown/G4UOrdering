a05f33c6a07db4c6229d63e59ab77a24580d7741
Joseph Brown <84574944+JosephSBrown@users.noreply.github.com>
2022-02-09 09:45:55 +0000

    Implementation of External JavaScript Fl

---
+++ home.js
@@ -0,0 +1,16 @@
+function darkmode() {
+    var body = document.body;
+    body.classList.toggle('dark-mode');
+}
+
+function userclick() {
+document.getElementById("userdrop").classList.toggle("usershow");
+}
+
+function notifclick() {
+document.getElementById("notifdrop").classList.toggle("notifshow");
+}
+
+function menuclick(x) {
+    x.classList.toggle("change");
+}
